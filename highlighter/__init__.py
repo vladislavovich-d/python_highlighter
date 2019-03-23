67f27fad37a6ab00a893f2e9ab2e795adcede411
vladislavovich-d <vladislav110912@gmail.com>
2019-03-23 14:18:15 +0200

    MyPyHighlighter

--- highlighter/__init__.py
+++ highlighter/__init__.py
@@ -1,14 +1,9 @@
-"""Flask module
-file: __init__.py
-date: 12.12.2012
-author smith@example.com
-license: MIT"""
-
 from flask import Flask, render_template, request, Markup
+import re
 
 
 def create_app():
-    """Create flask app for binding."""
+
     app = Flask(__name__)
 
     template_file_name = 'index.html'
@@ -28,24 +23,23 @@ def create_app():
         return render_template(template_file_name, **result)
 
     def markup_text(text):
-        """Markup given text.
-        This is supplementary method that helps you to wrap marked text in tags.
-        @:param text - string text to be marked
-        @:return marked text, e.g., <mark>highlighted text</mark>."""
+
         result = text
 
         # TODO: add an implementation
 
+        result = "<mark>{}</mark>".format(text)
         return result
 
     def highlight_text(text, expr):
-        """Markup searched string in given text.
-        @:param text - string text to be processed (e.g., 'The sun in the sky')
-        @:param expr - string pattern to be searched in the text (e.g., 'th')
-        @:return marked text, e.g., "<mark>Th</mark>e sun in <mark>th</mark>e sky"."""
+
         result = text
 
         # TODO: add an implementation
+
+        matchs = set(re.findall(expr, text, flags=re.IGNORECASE))
+        for i in matchs:
+            result = re.sub(i, markup_text(i), result)
 
         return result
 
